9272dc37a845be1b6bea118c91931ae18a8e7cd8
ManinaAnastasia <73671910+ManinaAnastasia@users.noreply.github.com>
2020-11-02 21:36:41 +0300

    Add files via upload

---
+++ Лаба 17/cpCF/cpCF/cpCF.cpp
@@ -0,0 +1,18 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <locale.h>
+int main(int argc, LPTSTR argv[])
+{
+	if (argc != 3)
+	{
+		setlocale(LC_ALL, ".1251");
+		printf("Использование: cpCF file1 file2\n");
+		return 1;
+	}
+	if (!CopyFile(argv[1], argv[2], FALSE))
+	{
+		printf("Ошибка CopyFile : % x\n", GetLastError());
+		return 2;
+	}
+	return 0;
+}
