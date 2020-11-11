04d9907560fcfaab4be100c96bb3700a55791655
ManinaAnastasia <73671910+ManinaAnastasia@users.noreply.github.com>
2020-11-11 16:16:25 +0300

    Add files via upload

---
+++ Лаба 20/MathFuncsDll/MyMathFuncs .cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "MathFuncsDll.h"
+#include <stdexcept>
+using namespace std;
+namespace MathFuncs
+{
+	double MyMathFuncs::Add(double a, double b)
+	{
+		return a + b;
+	}
+	double MyMathFuncs::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+	double MyMathFuncs::Multiply(double a, double b)
+	{
+		return 2* a * b;
+	}
+	double MyMathFuncs::Divide(double a, double b)
+	{
+		if (b == 0)
+		{
+			throw invalid_argument("b cannot be zero!");
+		}
+		return a / b;
+	}
+	double MyMathFuncs::Pow(double a, double b)
+	{
+		return a * a + b * b;
+	}
+}
---
+++ Лаба 20/MyExecRefsDll/MyExecRefsDll.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include<math.h>
+#include<locale.h>
+#include "MathFuncsDll.h"
+using namespace std;
+#define P 3.14159
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+	double a,f ,b,c,p,s;
+	
+	cout << "Введите сторону a  = ";
+	cin >> a;
+	cout << "Введите сторону  b = " ;
+	cin >> b;
+	cout << "Введите угол f = " ;
+	cin >> f ;
+		
+	if ((f >= 180) || (f <= 0))
+	{
+		cout << "Недопустимый угол";
+	}
+	else
+	{
+		c = sqrt(MathFuncs::MyMathFuncs::Pow(a, b)  -  MathFuncs::MyMathFuncs::Multiply(a, b) * cos(P * f / 180));
+		cout<<"Сторона с= "<< c <<endl;
+		p = MathFuncs::MyMathFuncs::Add(a, b) + c;
+		cout << "Периметр = " << p<<endl;
+		p = p / 2;
+		cout << "Полупериметр=" << p << endl;
+		s = sqrt(p * (p - a) * (p - b) * (p - c));
+		cout << "Площадь = " << s;
+	}
+	return 0;
+}
